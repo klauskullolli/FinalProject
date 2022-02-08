cdae1af7087c631487c2a57f7d4e9b6a11b8a312
KlausKullolliWolftechnologies <klaus.kullolli@wolftechnologies.al>
2022-02-08 09:26:35 +0100

    update

--- sqlQuery/user.sql
+++ sqlQuery/user.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS `user` (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deleted tinyint(1) default false
-)  ENGINE=INNODB;
+    role VARCHAR(255) NOT NULL
+    )  ENGINE=INNODB;
 
-INSERT INTO `user` (`username`, `password`,`created_at`, `deleted`) VALUES ('cindy', 'sindi123', current_timestamp(), '0');
-INSERT INTO `user` (`username`, `password`,`created_at`, `deleted`) VALUES ('enco', 'enco123', current_timestamp(), '0');
+INSERT INTO `user` (`username`, `password`,`created_at`, `role`) VALUES ('cindy', 'sindi123', current_timestamp(), 'user');
+INSERT INTO `user` (`username`, `password`,`created_at`, `role`) VALUES ('klaus', 'klaus123', current_timestamp(), 'admin');
